db7e17e26c512b9da86d3601392cbd9f51ce0f83
lexuanhai <emcuahai@gmail.com>
2023-02-13 09:45:08 +0700

    create project

---
+++ PhanMemQLKho/PhanMemQLKho/frmMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace PhanMemQLKho
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.ribbon1 = new System.Windows.Forms.Ribbon();
+            this.ribHeThong = new System.Windows.Forms.RibbonTab();
+            this.ribbonPanel1 = new System.Windows.Forms.RibbonPanel();
+            this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
+            this.ribSanPham = new System.Windows.Forms.RibbonTab();
+            this.ribKhoHang = new System.Windows.Forms.RibbonTab();
+            this.ribbonTab1 = new System.Windows.Forms.RibbonTab();
+            this.ribBaoCaoThongKe = new System.Windows.Forms.RibbonTab();
+            this.SuspendLayout();
+            // 
+            // ribbon1
+            // 
+            this.ribbon1.CaptionBarVisible = false;
+            this.ribbon1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.ribbon1.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.Minimized = false;
+            this.ribbon1.Name = "ribbon1";
+            // 
+            // 
+            // 
+            this.ribbon1.OrbDropDown.BorderRoundness = 8;
+            this.ribbon1.OrbDropDown.Location = new System.Drawing.Point(0, 0);
+            this.ribbon1.OrbDropDown.Name = "";
+            this.ribbon1.OrbDropDown.Size = new System.Drawing.Size(527, 447);
+            this.ribbon1.OrbDropDown.TabIndex = 0;
+            this.ribbon1.RibbonTabFont = new System.Drawing.Font("Trebuchet MS", 9F);
+            this.ribbon1.Size = new System.Drawing.Size(880, 107);
+            this.ribbon1.TabIndex = 0;
+            this.ribbon1.Tabs.Add(this.ribHeThong);
+            this.ribbon1.Tabs.Add(this.ribSanPham);
+            this.ribbon1.Tabs.Add(this.ribKhoHang);
+            this.ribbon1.Tabs.Add(this.ribbonTab1);
+            this.ribbon1.Tabs.Add(this.ribBaoCaoThongKe);
+            this.ribbon1.Text = "ribbon1";
+            // 
+            // ribHeThong
+            // 
+            this.ribHeThong.Name = "ribHeThong";
+            this.ribHeThong.Panels.Add(this.ribbonPanel1);
+            this.ribHeThong.Text = "Quản lý hệ thống";
+            // 
+            // ribbonPanel1
+            // 
+            this.ribbonPanel1.Items.Add(this.ribbonButton1);
+            this.ribbonPanel1.Name = "ribbonPanel1";
+            this.ribbonPanel1.Text = "";
+            // 
+            // ribbonButton1
+            // 
+            this.ribbonButton1.Image = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.Image")));
+            this.ribbonButton1.LargeImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.LargeImage")));
+            this.ribbonButton1.MinimumSize = new System.Drawing.Size(80, 0);
+            this.ribbonButton1.Name = "ribbonButton1";
+            this.ribbonButton1.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton1.SmallImage")));
+            this.ribbonButton1.Text = "Vai Trò";
+            // 
+            // ribSanPham
+            // 
+            this.ribSanPham.Name = "ribSanPham";
+            this.ribSanPham.Text = "Quản lý sản phẩm";
+            // 
+            // ribKhoHang
+            // 
+            this.ribKhoHang.Name = "ribKhoHang";
+            this.ribKhoHang.Text = "Quản lý kho hàng";
+            // 
+            // ribbonTab1
+            // 
+            this.ribbonTab1.Name = "ribbonTab1";
+            this.ribbonTab1.Text = "ribbonTab1";
+            // 
+            // ribBaoCaoThongKe
+            // 
+            this.ribBaoCaoThongKe.Name = "ribBaoCaoThongKe";
+            this.ribBaoCaoThongKe.Text = "Báo cáo thống kê";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 450);
+            this.Controls.Add(this.ribbon1);
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Ribbon ribbon1;
+        private System.Windows.Forms.RibbonTab ribHeThong;
+        private System.Windows.Forms.RibbonPanel ribbonPanel1;
+        private System.Windows.Forms.RibbonTab ribSanPham;
+        private System.Windows.Forms.RibbonTab ribKhoHang;
+        private System.Windows.Forms.RibbonTab ribBaoCaoThongKe;
+        private System.Windows.Forms.RibbonButton ribbonButton1;
+        private System.Windows.Forms.RibbonTab ribbonTab1;
+    }
+}
+
